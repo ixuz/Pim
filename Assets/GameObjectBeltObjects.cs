6286df5f53b3b47f5ce7159c00651828771fb421
Anton Wiklund <ixuz07@gmail.com>
2018-08-12 20:58:06 +0200

    GameOver scene

---
+++ Assets/GameObjectBeltObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameObjectBeltObjects : MonoBehaviour {
+
+    public Animator animator;
+    public Sprite[] sprites;
+    public Image itemImage;
+
+    private bool changeSpriteTriggered = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AnimatorStateInfo animatorStateInfo = animator.GetNextAnimatorStateInfo(0);
+        if (!changeSpriteTriggered && animatorStateInfo.IsName("ChangeSprite")) {
+            changeSpriteTriggered = true;
+            itemImage.sprite = sprites[Random.Range(0, sprites.Length)];
+        } else if (changeSpriteTriggered && !animatorStateInfo.IsName("ChangeSprite")) {
+            changeSpriteTriggered = false;
+        }
+    }
+}
